7ac5e5177755f47c351a8651d5b01d9c85923958
Mateusz Stankowski <m.stankowski16@gmail.com>
2021-08-27 00:32:08 +0200

    Fixes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
    var apiRoot = 'https://shielded-meadow-35621.herokuapp.com/v1/task/';
-   var trelloApiRoot = 'https://shielded-meadow-35621.herokuapp.com/v1/trello/';
+   //var trelloApiRoot = 'https://shielded-meadow-35621.herokuapp.com/v1/trello/';
    var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
-   var $tasksContainer = $('[data-tasks-container]');
+   var tasksContainer = $('[data-tasks-container]');
 
    var availableBoards = {};
    var availableTasks = {};
